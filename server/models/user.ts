730051496aae0f7565a1fd31999b5b379aa95ea2
ahrasel <ahrasel95@gmail.com>
2023-11-30 18:24:50 +0600

    user model updated

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,23 +1,25 @@
 import mongoose from "mongoose";
 
-export interface IUrl {
-  url: string;
-  slug: string;
-  clicks: number;
-  user: mongoose.Schema.Types.ObjectId;
-  urls: mongoose.Schema.Types.ObjectId[];
+export interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+  username: string;
+  urls?: mongoose.Schema.Types.ObjectId[];
 }
 
 const Schema = mongoose.Schema;
 
-const urlSchema = new Schema<IUrl>({
-  url: { type: String, required: [true, "URL is required"] },
-  slug: { type: String, required: [true, "Slug is required"], unique: true },
-  clicks: { type: Number, required: false, default: 0 },
-  user: { type: mongoose.Schema.Types.ObjectId, required: [true, "User is required"], ref: "User" },
-  urls: [{ type: mongoose.Schema.Types.ObjectId, ref: "Url" }],
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: [true, "Name is required"] },
+  email: { type: String, required: [true, "Email is required"], unique: true },
+  password: { type: String, required: [true, "Password is required"] },
+  avatar: { type: String, required: false, unique: true, sparse: true },
+  username: { type: String, required: [true, "Username is required"], unique: true },
+  urls: [{ type: mongoose.Schema.Types.ObjectId, ref: "Url", required: false }],
 });
 
-const Url = mongoose.model("Url", urlSchema);
+const User = mongoose.model("User", userSchema);
 
-export default Url;
+export default User;
